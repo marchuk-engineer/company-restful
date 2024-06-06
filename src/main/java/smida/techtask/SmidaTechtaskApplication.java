875ca060308144962db5141153dd97c15230633e
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-06-06 16:15:05 +0300

    Initial commit

---
+++ src/main/java/smida/techtask/SmidaTechtaskApplication.java
@@ -0,0 +1,13 @@
+package smida.techtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmidaTechtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmidaTechtaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smida/techtask/SmidaTechtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package smida.techtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmidaTechtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
